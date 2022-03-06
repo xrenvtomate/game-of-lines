cf722a61c18bca7c8d2c8808b6a49f4507917993
xrenvtomate <99559238+xrenvtomate@users.noreply.github.com>
2022-03-06 16:19:48 +0300

    Add files via upload
    it needs pygame

---
+++ logic.py
@@ -0,0 +1,106 @@
+import pygame as pg
+from pygame.math import Vector2
+import random
+
+pg.init()
+
+g = 0.2
+ball_radius = 20
+ball_color = '#D25EFF'
+ball_shadow ='#734884'
+bg_color = '#350044'
+line_color = '#5EFFFB'
+newline_color = '#2E7F7D'
+linebord_color = '#00C3CD'
+
+parc_group = pg.sprite.Group()
+
+def get_offset(x, width):
+    return width / 2 - x
+
+def offseted(dx, cord):
+    return cord[0] + dx, cord[1]
+
+def drawing(sc, ball, width, newxy, linepos, tail):
+    dx = get_offset(ball.x, width)
+    for el in tail: # drawing tail of ball
+        pg.draw.rect(sc, el.color, (el.x - el.size + dx, el.y - el.size, el.size * 2, el.size * 2))
+
+    #drawing parc
+    for el in parc_group:
+        pg.draw.rect(sc, el.color, (el.x - el.size + dx, el.y - el.size, el.size * 2, el.size * 2))
+
+    # ball
+    pg.draw.circle(sc, ball_shadow, (ball.x + dx, ball.y), ball_radius + 3)
+    pg.draw.circle(sc, ball_color, (ball.x + dx, ball.y), ball_radius)
+
+
+    if newxy is not None:
+        pg.draw.line(sc, newline_color, offseted(dx, newxy), pg.mouse.get_pos(), 5)
+
+    if linepos != (None, None):
+        # line
+        pg.draw.line(sc, linebord_color, offseted(dx, linepos[0]), offseted(dx, linepos[1]), 12)
+        pg.draw.line(sc, line_color, offseted(dx, linepos[0]), offseted(dx, linepos[1]), 8)
+
+        # line borders
+        pg.draw.circle(sc, linebord_color, offseted(dx, linepos[0]), 10)
+        pg.draw.circle(sc, linebord_color, offseted(dx, linepos[1]), 10)
+
+
+def draw_parc(x, y, sc):
+        for i in range(30):
+            Parc(x, y)
+
+class Ball(pg.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.x = 0
+        self.y = 200
+        self.direction = Vector2(0, -10)
+
+    def update(self, linepos, sc):
+        # moving
+        self.direction.y += g
+        self.x += self.direction.x
+        self.y += self.direction.y
+
+        # collision
+
+        if linepos != (None, None):
+            rect = pg.Rect(self.x - ball_radius, self.y - ball_radius, 2 * ball_radius, 2 * ball_radius) # ball rect
+            if rect.clipline(linepos):
+                line_norm = Vector2(-linepos[1][1] + linepos[0][1], linepos[1][0] - linepos[0][0])
+                self.direction = self.direction.reflect(line_norm)
+                self.direction *= 1.01
+                draw_parc(self.x, self.y, sc)
+
+
+class Tailparticle():
+    def __init__(self, x, y):
+        self.size = random.randint(1, 4)
+        self.x = random.randint(x - ball_radius + 5, x + ball_radius - 5)
+        self.y = random.randint(y - ball_radius + 5, y + ball_radius - 5)
+        self.color = random.choice(('#00D1F6', '#00B3D2', '#0182B0'))
+
+
+class Parc(pg.sprite.Sprite):
+    def __init__(self, x, y):
+        super().__init__(parc_group)
+        self.color = random.choice(('#E25EFF', '#7D00C5', '#C200C5'))
+        self.x, self.y = x, y
+        self.vx = random.randint(-10, 10)
+        self.vy = random.randint(-10, -1)
+        self.size = random.randint(1, 3)
+
+    def update(self): # moving
+        self.x += self.vx
+        self.y += self.vy
+        self.vy += 2 * g
+        if self.y > 2000:
+            del self
+
+
+class Line():
+    def __init__(self, start, end):
+        self.cord = start, end
---
+++ main.py
@@ -0,0 +1,47 @@
+from logic import *
+
+
+font = pg.font.Font(None, 100) # font settings
+WIDTH, HEIGHT = 1600, 900
+tail = []
+clock = pg.time.Clock()
+sc = pg.display.set_mode((WIDTH, HEIGHT))
+
+newxy = None
+ball = Ball()
+line = Line(None, None)
+running = True
+while running:
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            running = False
+        elif event.type == pg.MOUSEBUTTONDOWN:
+            if newxy is None:
+                newxy = offseted(-get_offset(ball.x, WIDTH), event.pos)
+            else:
+                line = Line(newxy, offseted(-get_offset(ball.x, WIDTH), event.pos))
+                newxy = None
+        elif event.type == pg.KEYDOWN:
+            if event.key == pg.K_ESCAPE:
+                running = False
+            elif event.key == pg.K_r:
+                # clear all
+                tail = []
+                newxy = None
+                ball = Ball()
+                line = Line(None, None)
+
+
+    ball.update(line.cord, sc)
+
+
+    parc_group.update()
+    tail.append(Tailparticle(int(ball.x), int(ball.y)))
+    if len(tail) > 180:
+        tail = tail[1:]
+    sc.fill(bg_color)
+    drawing(sc, ball, WIDTH, newxy, line.cord, tail)
+    if ball.y > HEIGHT + 50:
+        sc.blit(font.render('press R to restert', 1, '#58FF8D'), (100, 100))
+    pg.display.flip()
+    clock.tick(60)
